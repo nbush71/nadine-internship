7abff5b9bc0a4ce0777b90422b5568d71d9ac5b9
nbush71 <nbush71@gmail.com>
2026-08-29 14:21:35 -0500

    Added Arrow Style and CSS

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -48,45 +48,47 @@ const HotCollections = () => {
             </div>
           </div>
         </div>
-        <button onClick={() => instanceRef.current?.prev()} className="d-nav-left">
-          <i className="fa fa-chevron-left"></i>
-        </button>
-        <div ref={sliderRef} className="keen-slider">
-          {collections.map((collection, index) => (
-            <div className="keen-slider__slide" key={index}>
-              <div className="nft_coll">
-                <div className="nft_wrap">
-                  <Link to="/item-details">
-                    <img
-                      src={collection.nftImage}
-                      className="lazy img-fluid"
-                      alt=""
-                    />
-                  </Link>
-                </div>
-                <div className="nft_coll_pp">
-                  <Link to="/author">
-                    <img
-                      className="lazy pp-coll"
-                      src={collection.authorImage}
-                      alt=""
-                    />
-                  </Link>
-                  <i className="fa fa-check"></i>
-                </div>
-                <div className="nft_coll_info">
-                  <Link to="/explore">
-                    <h4>{collection.title}</h4>
-                  </Link>
-                  <span>{collection.code}</span>
+        <div className="hot-collections-slider">
+          <button onClick={() => instanceRef.current?.prev()} className="d-nav-left">
+            <i className="fa fa-chevron-left"></i>
+          </button>
+          <div ref={sliderRef} className="keen-slider">
+            {collections.map((collection, index) => (
+              <div className="keen-slider__slide" key={index}>
+                <div className="nft_coll">
+                  <div className="nft_wrap">
+                    <Link to="/item-details">
+                      <img
+                        src={collection.nftImage}
+                        className="lazy img-fluid"
+                        alt=""
+                      />
+                    </Link>
+                  </div>
+                  <div className="nft_coll_pp">
+                    <Link to="/author">
+                      <img
+                        className="lazy pp-coll"
+                        src={collection.authorImage}
+                        alt=""
+                      />
+                    </Link>
+                    <i className="fa fa-check"></i>
+                  </div>
+                  <div className="nft_coll_info">
+                    <Link to="/explore">
+                      <h4>{collection.title}</h4>
+                    </Link>
+                    <span>{collection.code}</span>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
+          <button onClick={() => instanceRef.current?.next()} className="d-nav-right">
+            <i className="fa fa-chevron-right"></i>
+          </button>
         </div>
-        <button onClick={() => instanceRef.current?.next()} className="d-nav-right">
-          <i className="fa fa-chevron-right"></i>
-        </button>
       </div>
     </section>
   );
