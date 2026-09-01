75ebb15ad27063870b6235be228e96d02e2a6ae8
nbush71 <nbush71@gmail.com>
2026-09-01 11:54:42 -0500

    Added keen-slider and arrows

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -1,8 +1,28 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+//import Skeleton from "../UI/Skeleton";
+import { useKeenSlider } from "keen-slider/react";
+import "keen-slider/keen-slider.min.css";
 
 const NewItems = () => {
-  const [items , setItems] = useState([]);
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const [sliderRef, instanceRef] = useKeenSlider({
+    loop: true,
+    slides: {
+      perView: 4,
+      spacing: 12,
+    },
+    breakpoints: {
+      "(max-width: 768px)": {
+        slides: { perView: 1, spacing: 10 },
+      },
+      "(max-width: 1024px)": {
+        slides: { perView: 2, spacing: 10 },
+      },
+    },
+  });
 
   async function fetchItems() {
     const response = await fetch(
@@ -10,9 +30,7 @@ const NewItems = () => {
     );
 
     const data = await response.json();
-    console.log('THIS WORKS')
     setItems(data);
-
   }
 
   useEffect(() => {
@@ -29,65 +47,73 @@ const NewItems = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          
-          {items.slice(0, 4).map((item, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft__item">
-                <div className="author_list_pp">
-                  <Link
-                    to="/author"
-                    data-bs-toggle="tooltip"
-                    data-bs-placement="top"
-                    title="Creator: Monica Lucas"
-                  >
-                    <img className="lazy" src={item.authorImage} alt="" />
-                    <i className="fa fa-check"></i>
-                  </Link>
-                </div>
-                <div className="de_countdown">5h 30m 32s</div>
+          <div className="new-items-slider">
+            <button
+              onClick={() => instanceRef.current?.prev()}
+              className="d-nav-left"
+            >
+              <i className="fa fa-chevron-left"></i>
+            </button>
+            <div ref={sliderRef} className="keen-slider">
+              {items.slice(0, 4).map((item, index) => (
+                <div className="keen-slider__slide" key={index}>
+                  <div className="nft__item">
+                    <div className="author_list_pp">
+                      <Link to="/author">
+                        <img className="lazy" src={item.authorImage} alt="" />
+                        <i className="fa fa-check"></i>
+                      </Link>
+                    </div>
+                    <div className="de_countdown">5h 30m 32s</div>
 
-                <div className="nft__item_wrap">
-                  <div className="nft__item_extra">
-                    <div className="nft__item_buttons">
-                      <button>Buy Now</button>
-                      <div className="nft__item_share">
-                        <h4>Share</h4>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-facebook fa-lg"></i>
-                        </a>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-twitter fa-lg"></i>
-                        </a>
-                        <a href="">
-                          <i className="fa fa-envelope fa-lg"></i>
-                        </a>
+                    <div className="nft__item_wrap">
+                      <div className="nft__item_extra">
+                        <div className="nft__item_buttons">
+                          <button>Buy Now</button>
+                          <div className="nft__item_share">
+                            <h4>Share</h4>
+                            <a href="" target="_blank" rel="noreferrer">
+                              <i className="fa fa-facebook fa-lg"></i>
+                            </a>
+                            <a href="" target="_blank" rel="noreferrer">
+                              <i className="fa fa-twitter fa-lg"></i>
+                            </a>
+                            <a href="">
+                              <i className="fa fa-envelope fa-lg"></i>
+                            </a>
+                          </div>
+                        </div>
+                      </div>
+
+                      <Link to="/item-details">
+                        <img
+                          src={item.nftImage}
+                          className="lazy nft__item_preview"
+                          alt=""
+                        />
+                      </Link>
+                    </div>
+                    <div className="nft__item_info">
+                      <Link to="/item-details">
+                        <h4>{item.title}</h4>
+                      </Link>
+                      <div className="nft__item_price">{item.price}</div>
+                      <div className="nft__item_like">
+                        <i className="fa fa-heart"></i>
+                        <span>{item.likes}</span>
                       </div>
                     </div>
                   </div>
-
-                  <Link to="/item-details">
-                    <img
-                      src={item.nftImage}
-                      className="lazy nft__item_preview"
-                      alt=""
-                    />
-                  </Link>
                 </div>
-                <div className="nft__item_info">
-                  <Link to="/item-details">
-                    <h4>{item.title}</h4>
-                  </Link>
-                  <div className="nft__item_price">{item.price}</div>
-                  <div className="nft__item_like">
-                    <i className="fa fa-heart"></i>
-                    <span>{item.likes}</span>
-                  </div>
-                </div>
-              </div>
+              ))}
             </div>
-          ))}
+            <button onClick={() => instanceRef.current?.next()}
+              className="d-nav-right">
+              <i className="fa fa-chevron-right"></i>
+            </button>
+          </div>
+          </div>
         </div>
-      </div>
     </section>
   );
 };
